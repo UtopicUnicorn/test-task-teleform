8dfa77ae397f4d1b1167d4627ca1ca052bcf4d55
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-08-04 16:10:25 +0700

    added form group and template layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Form, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  static phone_mask = ['(', /[1-9]/, /\d/, /\d/, ')', '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+
+  registationForm!: FormGroup;
   title = 'test-task-teleform';
+  step = 1;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void{
+    this.createForm();
+  }
+
+  createForm(): void{
+    this.registationForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      surname: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      phone: new FormControl('',[Validators.required, Validators.pattern('^[+][0-9]([0-9]{10})')]),
+      date: new FormControl('', Validators.required),
+      agree: new FormControl(null, Validators.required),
+      mailing: new FormControl(null, Validators.nullValidator),
+    })
+  }
+
+
+  nextStep(): void{
+    if(this.step+1>3){
+      this.step =3;
+      return;
+    }
+    this.step++;
+  }
+
+  previousStep(): void{
+    if(this.step-1<1){
+      this.step = 1;
+      return
+    }
+    this.step--;
+  }
+
+  sendForm():void{
+    console.log(this.registationForm.value);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    FormBuilder,
+    MatNativeDateModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
