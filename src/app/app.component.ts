e11529813f481d8723694b52865962c3f52409e7
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-08-10 13:15:01 +0700

    prettier fixes, deleted unused packages, and button localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,73 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {MatDialog} from "@angular/material/dialog";
-import {MapComponent} from "./map/map.component";
-import {pipe} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MapComponent } from './map/map.component';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   registrationForm!: FormGroup;
   address = '';
   title = 'test-task-teleform';
 
-  get formArray(): AbstractControl | null { return this.registrationForm.get('formArray'); }
-
-  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+  get formArray(): AbstractControl | null {
+    return this.registrationForm.get('formArray');
   }
 
-  ngOnInit(): void{
+  constructor(private fb: FormBuilder, public dialog: MatDialog) {}
+
+  ngOnInit(): void {
     this.createForm();
   }
 
-
-  createForm(): void{
+  createForm(): void {
     this.registrationForm = this.fb.group({
       formArray: this.fb.array([
-      this.fb.group({
-      name: new FormControl('', Validators.required),
-      surname: new FormControl('', Validators.required),
-    }),
-      this.fb.group({
-      email: new FormControl('', [Validators.email, Validators.required]),
-      phone: new FormControl('',[Validators.required, Validators.pattern('^([+][0-9])|([0-9])([0-9]{10})')]),
-      address: new FormControl('', [Validators.required]),
-      date: new FormControl('', Validators.required),
-    }),
-      this.fb.group({
-      agree: new FormControl(false, Validators.requiredTrue),
-      mailing: new FormControl(false, Validators.nullValidator),
-    })
-      ])
-
-    })
+        this.fb.group({
+          name: new FormControl('', Validators.required),
+          surname: new FormControl('', Validators.required),
+        }),
+        this.fb.group({
+          email: new FormControl('', [Validators.email, Validators.required]),
+          phone: new FormControl('', [
+            Validators.required,
+            Validators.pattern('^([+][0-9])|([0-9])([0-9]{10})'),
+          ]),
+          address: new FormControl('', [Validators.required]),
+          date: new FormControl('', Validators.required),
+        }),
+        this.fb.group({
+          agree: new FormControl(false, Validators.requiredTrue),
+          mailing: new FormControl(false, Validators.nullValidator),
+        }),
+      ]),
+    });
   }
 
-
-
-
-  openMap(){
+  openMap() {
     let dialog = this.dialog.open(MapComponent);
-    dialog.afterClosed().subscribe(pipe((result: string)=>this.address = result, ()=>this.registrationForm.get('formArray.1.address')?.patchValue(this.address)));
+    dialog.afterClosed().subscribe(
+      pipe(
+        (result: string) => (this.address = result),
+        () =>
+          this.registrationForm
+            .get('formArray.1.address')
+            ?.patchValue(this.address)
+      )
+    );
   }
 
-  sendForm():void{
+  sendForm(): void {
     console.log(this.registrationForm.value);
   }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatNativeDateModule } from "@angular/material/core";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatButtonModule } from "@angular/material/button";
-import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MapComponent } from './map/map.component';
-import { MatDialogModule } from "@angular/material/dialog";
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MapComponent,
-  ],
+  declarations: [AppComponent, MapComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -32,10 +29,7 @@ import { MatStepperModule } from '@angular/material/stepper';
     MatDialogModule,
     MatStepperModule,
   ],
-  providers: [
-    FormBuilder,
-    MatNativeDateModule,
-  ],
-  bootstrap: [AppComponent]
+  providers: [FormBuilder, MatNativeDateModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
